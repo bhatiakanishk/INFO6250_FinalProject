7fbb507d34e0c1351c914ddf1a8768f0b97140a1
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-12 18:30:41 -0400

    Create Admin.java

---
+++ FinalProject/src/main/java/com/mycompany/pojo/Admin.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pojo;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author kanishk
+ */
+
+//Admin POJO
+@Entity
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Basic
+    @Column(name = "email")
+    private String email;
+    
+    @Basic
+    @Column(name = "pass")
+    private String pass;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    public Admin(){}
+}
