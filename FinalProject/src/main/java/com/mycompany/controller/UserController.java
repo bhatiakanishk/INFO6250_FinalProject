77f5d66685ecd8a52d23992f619f6ad9bc9c6e62
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-14 11:34:03 -0400

    Create UserController.java

---
+++ FinalProject/src/main/java/com/mycompany/controller/UserController.java
@@ -0,0 +1,229 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.controller;
+
+import com.mycompany.dao.TicketDao;
+import com.mycompany.dao.UserDao;
+import com.mycompany.exception.FlightException;
+import com.mycompany.pojo.Ticket;
+import com.mycompany.pojo.User;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author kanishk
+ */
+@Controller
+@SessionAttributes("userLogin")
+public class UserController {
+
+    Random rand;
+    Date date = new Date();
+
+    // Login Page
+    @GetMapping("userLogin.htm")
+    public String showLoginForm(ModelMap model, User user) {
+        //User user = new User();
+        model.addAttribute("user", user);
+        return "loginPage";
+    }
+
+    @PostMapping("userLogin.htm")
+    public ModelAndView handleLogin(@ModelAttribute("user") User user, BindingResult result, SessionStatus status, UserDao userdao, HttpSession session) throws FlightException {
+
+        if (result.hasErrors()) {
+            ModelAndView model = new ModelAndView("register");
+            return model;
+        } else {
+
+            User retrievedUser = userdao.get(user);
+            System.out.println(retrievedUser);
+            if (retrievedUser != null) {
+                session.setAttribute("user", retrievedUser);
+                status.setComplete();
+                ModelAndView model = new ModelAndView("userWelcome");
+                return model;
+            } else {
+                ModelAndView model = new ModelAndView("error");
+                return model;
+            }
+        }
+
+    }
+
+    @GetMapping("userRegister.htm")
+    public String showRegistrationForm(ModelMap model) {
+        User user = new User(); // FormBackingObject
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("userRegister.htm")
+    public String handleRegistrationForm(ModelMap model, @ModelAttribute("user") User user, BindingResult result, SessionStatus status, UserDao userdao) throws FlightException {
+        if (result.hasErrors()) {
+            return "register";
+        } else {
+            User alreadyUser = userdao.get(user);
+
+            if (alreadyUser == null) {
+                if (user.getAge() <= 0 && user.getContactNo() <= 99999999) {
+                    return "registerError";
+                }
+                userdao.create(user);
+                status.setComplete();
+                return "addedUser";
+            } else {
+                model.addAttribute("user", alreadyUser);
+                return "registerError";
+            }
+
+        }
+    }
+
+    // Logout 
+    @RequestMapping("logout.htm")
+    public ModelAndView getLogout(HttpSession session) {
+        session.invalidate();
+        ModelAndView model = new ModelAndView("redirect:/");
+        return model;
+    }
+
+    @GetMapping("bookTicket.htm")
+    public String showAdminFlightForm(ModelMap model, Ticket ticket) {
+
+        model.addAttribute("ticket", ticket);
+        return "bookTicket";
+    }
+
+    @PostMapping("bookTicket.htm")
+    public String handleCreateFlightForm(ModelMap model, @ModelAttribute("ticket") Ticket ticket, BindingResult result, SessionStatus status, TicketDao ticketDao) throws FlightException {
+
+        if (result.hasErrors()) {
+            return "bookTicket";
+        } else if (ticket.getTravel_date().before(date)) {
+
+//        } else if(ticket.getTravel_date() < date){
+            return "flightError";
+        } else {
+            int num = (int) System.currentTimeMillis();
+//            int PRN = rand.nextInt((num) + 1);
+
+            ticket.setPNR(Math.abs(num));
+            ticket.setDate(date);
+            Ticket alreadyTicket = ticketDao.get(ticket);
+
+            if (alreadyTicket == null) {
+                ticketDao.create(ticket);
+                status.setComplete();
+                return "flightDataAdded";
+            } else {
+                model.addAttribute("flight", alreadyTicket);
+                return "flightError";
+            }
+        }
+    }
+
+    @GetMapping("closeAccount.htm")
+    public String showDeleteUser(ModelMap model, User user) {
+
+        model.addAttribute("user", user);
+        return "closeUser";
+    }
+
+    @PostMapping("closeAccount.htm")
+    public String handleDeleteUser(ModelMap model, @ModelAttribute("user") User user, BindingResult result, SessionStatus status, UserDao userDao) throws FlightException {
+        if (result.hasErrors()) {
+            return "closeUser";
+        } else {
+//            String backEmail = user.getEmail();
+            User alreadyUser = userDao.getforDelete(user);
+            if (alreadyUser != null) {
+                userDao.delete(alreadyUser);
+                status.setComplete();
+                return "deleteUserSuccess";
+            } else {
+                model.addAttribute("flight", alreadyUser);
+                return "flightError";
+            }
+
+        }
+
+    }
+
+    @GetMapping("deleteTicket.htm")
+    public String deleteUserTicketForm(ModelMap model, Ticket ticket) {
+
+        model.addAttribute("ticket", ticket);
+        return "deleteTicket";
+    }
+
+    @PostMapping("deleteTicket.htm")
+    public String handleDeleteAdminUsersForm(ModelMap model, @ModelAttribute("ticket") Ticket ticket, BindingResult result, SessionStatus status, TicketDao ticketDao) throws FlightException {
+        if (result.hasErrors()) {
+            return "deleteTicket";
+        } else {
+            Ticket alreadyTicket = ticketDao.get(ticket);
+            if (alreadyTicket != null) {
+                ticketDao.delete(alreadyTicket);
+                status.setComplete();
+                return "deleteTicketSuccess";
+            } else {
+                model.addAttribute("flight", alreadyTicket);
+                return "flightError";
+            }
+        }
+    }
+
+    @GetMapping("updateUser1.htm")
+    public String updateAdminUsersForm(ModelMap model, User user) {
+
+        model.addAttribute("user", user);
+        return "editUserData_1";
+    }
+
+    @PostMapping("updateUser1.htm")
+    public String handleEditAdminUserForm(ModelMap model, @ModelAttribute("user") User user, BindingResult result, SessionStatus status, UserDao userDao) throws FlightException {
+        if (result.hasErrors()) {
+            return "editUserData_1";
+        } else {
+
+            User alreadyUser = userDao.getforDelete(user);
+
+//            user.setId(alreadyUser.getId());
+            if (alreadyUser != null) {
+                userDao.delete(alreadyUser);
+                userDao.create(user);
+                status.setComplete();
+                return "updateUserSuccess";
+            } else {
+                model.addAttribute("user", alreadyUser);
+                return "userError";
+            }
+        }
+
+    }
+
+    @GetMapping("viewAllBookings.htm")
+    public String showUserInformation(HttpServletRequest request, TicketDao ticketDao, HttpSession session) throws FlightException {
+        List<Ticket> ticketList = ticketDao.getAll((User) (session.getAttribute("user")));
+        request.setAttribute("ticketList", ticketList);
+        return "viewBookingData";
+    }
+
+}
