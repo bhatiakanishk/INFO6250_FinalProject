8a106a08daf03721ea5974a0ce9a5d455b40baf1
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-04-14 00:39:50 -0400

    Create FlightException.java

---
+++ FinalProject/src/main/java/com/mycompany/exception/FlightException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exception;
+
+/**
+ *
+ * @author kanishk
+ */
+public class FlightException extends Exception{
+    
+    public FlightException(String message){
+        super(message);
+    }
+    
+    public FlightException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
